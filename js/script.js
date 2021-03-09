e3a2cb5ecca7658e8af7f9179be22691d6830045
lisaankerrasch <lisa.ar.strom@gmail.com>
2021-03-09 21:45:09 +0100

    add to cart stuff

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const cartButton = document.querySelector(".add-cart");
+const successMessage = document.querySelector(".success-message");
+const loaderContainer = document.querySelector(".loader-container");
+const cartCount = document.querySelector(".cart-count");
+
+cartButton.onclick = function addToCart() {
+  cartButton.classList.add("hiddenjs");
+  loaderContainer.classList.add("loader");
+  setTimeout(function () {
+    loaderContainer.classList.remove("loader");
+    successMessage.innerHTML = `<div class="success">The item was added to your cart!</div>`;
+    cartCount.classList.add("cart-count-1");
+    cartCount.innerHTML = `1`;
+    console.log("Success!");
+  }, 300);
+  return false;
+};
