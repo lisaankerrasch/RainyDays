6dbb8a0c53379166df40d12dd715a1dab60abb3c
lisaankerrasch <lisa.ar.strom@gmail.com>
2021-03-11 16:17:06 +0100

    searchbar and nav

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,12 @@
 const shopNav = document.querySelector(".shopNav");
 const shopOverlay = document.querySelector(".overlay-shop");
+const aboutNav = document.querySelector(".aboutNav");
+const aboutOverlay = document.querySelector(".overlay-about");
+const searchIcon = document.querySelector(".search-icon");
+const searchbar = document.querySelector(".searchbar");
 
 shopNav.onmouseover = function () {
-  setTimeout(function () {
-    shopOverlay.classList.remove("hiddenjs");
-  }, 300);
+  shopOverlay.classList.remove("hiddenjs");
 };
 
 shopOverlay.onmouseover = function () {
@@ -17,4 +19,24 @@ shopNav.onmouseout = function () {
 
 shopOverlay.onmouseout = function () {
   shopOverlay.classList.add("hiddenjs");
+};
+
+aboutNav.onmouseover = function () {
+  aboutOverlay.classList.remove("hiddenjs");
+};
+
+aboutOverlay.onmouseover = function () {
+  aboutOverlay.classList.remove("hiddenjs");
+};
+
+aboutNav.onmouseout = function () {
+  aboutOverlay.classList.add("hiddenjs");
+};
+
+aboutOverlay.onmouseout = function () {
+  aboutOverlay.classList.add("hiddenjs");
+};
+
+searchIcon.onclick = function () {
+  searchbar.classList.remove("hiddenjs");
 };
